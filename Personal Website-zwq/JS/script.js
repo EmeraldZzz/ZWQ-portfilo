97568ec3923baaaadbbaf05833ee61a85cfaeeef
EmeraldZzz <49337056+EmeraldZzz@users.noreply.github.com>
2022-10-24 22:20:01 +0800

    Delete Personal Website-zwq directory

--- Personal Website-zwq/JS/script.js
+++
@@ -1,7 +0,0 @@
-
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".header-container");
-
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
-});
